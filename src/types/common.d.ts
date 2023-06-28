694661e5422214266545d12b8db650266ca8b3db
Okysu <yby@ecanse.com>
2023-06-28 17:53:01 +0800

    feat:more comprehensive reactive system

--- src/types/common.d.ts
+++ src/types/common.d.ts
@@ -1,10 +1,29 @@
 // keys<T> is a type that extracts all keys from T that are string or number.
 type keys<T> = T extends object ? Extract<keyof T, string | number> : never
 
-// Instance type of a class
+// instance type of a class
 type SeaSideJSOptions = {
     template: string | DocumentFragment
     data: () => object
-    methods: { [key: string]: (...args: any[]) => any }
+    methods?: { [key: string]: (...args: any[]) => any }
     mounted?: () => void
+}
+
+// proxy interface
+interface effect {
+    (): any
+    deps: Set<effect>[]
+    options?: effectOptions
+}
+// effect options type
+type effectOptions = Object & {
+    scheduler?: Function
+    lazy?: boolean
+}
+
+// watch options type
+type watchOptions = Object & {
+    immediate?: boolean
+    deep?: boolean
+    flush?: string
 }
--- test/reactive.test.ts
+++ test/reactive.test.ts
@@ -1,4 +1,4 @@
-import { reactive } from '../src/libs/reactive'
+import { reactive } from '../src/libs/proxy'
 
 
 const obj = reactive({
